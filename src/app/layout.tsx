7fdf298f3de158f5122e02651f3f172033fbb875
rifkifajarramadhani <rifkid56@gmail.com>
2024-09-27 21:20:55 +0700

    chore: update metadata & favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'John Doe',
+  title: 'Rifki | Full-Stack Engineer',
 }
 
 export default function RootLayout({
